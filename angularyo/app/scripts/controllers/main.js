a92a2e41ed17dabe96308a7172aebb88b42ecc96
Frédéric Delorme <frederic.delorme@gmail.com>
2016-02-15 02:03:30 +0100

    add an angular base on yeoman geneator.

---
+++ angularyo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularyoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularyoApp
+ */
+angular.module('angularyoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
