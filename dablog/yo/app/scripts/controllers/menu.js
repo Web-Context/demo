9e40bf6daadfce9cbfdd9404ffb350a104e7b0f1
'Frédéric <frederic.delorme@gmail.com>
2016-03-10 18:45:20 +0100

    - start implementing basic operations for authentication system. - fix issues on main menu and post display.

--- dablog/yo/app/scripts/controllers/menu.js
+++ dablog/yo/app/scripts/controllers/menu.js
@@ -20,12 +20,28 @@ angular.module('dablogApp')
         {'url':'/posts/game','label':'Video Game','tooltip':'How to feed my Console','accesskey':'G','priority':3},
     ];
 
-    $scope.user = {
-    	'url':'profile/00117898665765',
-    	'username':'McGivrer',
-    	'firstname':'Frédéric',
-    	'lastname':'Delorme',
-    	'email':'frederic.delorme@web-context.com',
-    	'avatar':'https://trello-avatars.s3.amazonaws.com/372550260169a4891d0c419fb7af6b87/170.png',
-    };
+
+    $scope.login = function(){
+        $scope.user = {
+            'url':'profile/00117898665765',
+            'username':'McGivrer',
+            'firstname':'Frédéric',
+            'lastname':'Delorme',
+            'email':'frederic.delorme@web-context.com',
+            'avatar':'https://trello-avatars.s3.amazonaws.com/372550260169a4891d0c419fb7af6b87/170.png',
+        };
+
+    }
+
+    $scope.logout = function(){
+        $scope.user=null;
+    }
+
+    $scope.preferences = function(){
+        alert("display preference dialog !");
+    }
+    $scope.register = function(){
+        alert("display register dialog !");
+    }
+
   }]);
