4ca1f8ff185ad5fd73351f5f054a06082656f5e5
'Frédéric <frederic.delorme@gmail.com>
2016-03-15 02:22:03 +0100

    Fix error with Lombok usage. Revert to POJO.

--- dablog/src/main/java/com/gehc/apps/demo/dablog/model/post/Post.java
+++ dablog/src/main/java/com/gehc/apps/demo/dablog/model/post/Post.java
@@ -24,7 +24,7 @@ import lombok.ToString;
  *
  */
 @Data
-@ToString(includeFieldNames=true)
+@ToString(includeFieldNames = true)
 public class Post {
 
 	@Id
@@ -57,4 +57,310 @@ public class Post {
 	private PublicationState state;
 
 	private Map<String, Object> metadata;
+
+	public Post() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id
+	 * @param uiid
+	 * @param title
+	 * @param type
+	 * @param header
+	 * @param content
+	 * @param createdAt
+	 * @param createdBy
+	 * @param publicationAt
+	 * @param state
+	 * @param metadata
+	 */
+	public Post(String id, String uiid, String title, String type, String header, String content, Date createdAt,
+			String createdBy, Date publicationAt, PublicationState state) {
+		super();
+		this.id = id;
+		this.uiid = uiid;
+		this.title = title;
+		this.type = type;
+		this.header = header;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.createdBy = createdBy;
+		this.publicationAt = publicationAt;
+		this.state = state;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the uiid
+	 */
+	public String getUiid() {
+		return uiid;
+	}
+
+	/**
+	 * @param uiid
+	 *            the uiid to set
+	 */
+	public void setUiid(String uiid) {
+		this.uiid = uiid;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the header
+	 */
+	public String getHeader() {
+		return header;
+	}
+
+	/**
+	 * @param header
+	 *            the header to set
+	 */
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content
+	 *            the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @param createdAt
+	 *            the createdAt to set
+	 */
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param createdBy
+	 *            the createdBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the publicationAt
+	 */
+	public Date getPublicationAt() {
+		return publicationAt;
+	}
+
+	/**
+	 * @param publicationAt
+	 *            the publicationAt to set
+	 */
+	public void setPublicationAt(Date publicationAt) {
+		this.publicationAt = publicationAt;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public PublicationState getState() {
+		return state;
+	}
+
+	/**
+	 * @param state
+	 *            the state to set
+	 */
+	public void setState(PublicationState state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the metadata
+	 */
+	public Map<String, Object> getMetadata() {
+		return metadata;
+	}
+
+	/**
+	 * @param metadata
+	 *            the metadata to set
+	 */
+	public void setMetadata(Map<String, Object> metadata) {
+		this.metadata = metadata;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Post [id=").append(id).append(", uiid=").append(uiid).append(", title=").append(title)
+				.append(", type=").append(type).append(", header=").append(header).append(", content=").append(content)
+				.append(", createdAt=").append(createdAt).append(", createdBy=").append(createdBy)
+				.append(", publicationAt=").append(publicationAt).append(", state=").append(state).append(", metadata=")
+				.append(metadata).append("]");
+		return builder.toString();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((createdBy == null) ? 0 : createdBy.hashCode());
+		result = prime * result + ((header == null) ? 0 : header.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((metadata == null) ? 0 : metadata.hashCode());
+		result = prime * result + ((publicationAt == null) ? 0 : publicationAt.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((uiid == null) ? 0 : uiid.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (createdBy == null) {
+			if (other.createdBy != null)
+				return false;
+		} else if (!createdBy.equals(other.createdBy))
+			return false;
+		if (header == null) {
+			if (other.header != null)
+				return false;
+		} else if (!header.equals(other.header))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (metadata == null) {
+			if (other.metadata != null)
+				return false;
+		} else if (!metadata.equals(other.metadata))
+			return false;
+		if (publicationAt == null) {
+			if (other.publicationAt != null)
+				return false;
+		} else if (!publicationAt.equals(other.publicationAt))
+			return false;
+		if (state != other.state)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (uiid == null) {
+			if (other.uiid != null)
+				return false;
+		} else if (!uiid.equals(other.uiid))
+			return false;
+		return true;
+	}
+
 }
